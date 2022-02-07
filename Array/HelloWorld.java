f440ef7a7187c09779f869a2d140ce9319f7316e
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-07 14:33:22 -0500

    Update HelloWorld.java

--- Array/HelloWorld.java
+++ Array/HelloWorld.java
@@ -1,11 +1,19 @@
-//am9634
+
 //Aigerim Madakimova
-
-public class HelloWorld {
-
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
+public enum Color {red, white, blue}
+public static void dutchFlagPartion (int pivotIndex, List<Color> A){
+	Color pivot = A.get(pivotIndex);
+	int smaller =0;
+	for(int i = 0; i < A.size(); ++i){
+		if(A.get(i).ordinal() < pivot.ordinal()){
+			Collections.swap(A, smaller++, i);
+		}
+	}
+	int larger = A.size()-1;
+	for(int i = A.size()-1; i>0; --i){
+		if(A.get(i).ordinal() > pivot.ordinal()){
+			Collections.swap(A, larger--, i);
+		}
 	}
 }
 
