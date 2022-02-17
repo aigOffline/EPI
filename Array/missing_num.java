9cdfb336d5bb71a6de450fbe502ffcb3218902be
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-16 22:09:12 -0500

    Update missing_num.java

--- Array/missing_num.java
+++ Array/missing_num.java
@@ -4,7 +4,7 @@ public class MyClass {
    public static void main(String args[]){
        int[] input = {  1, 1, 2, 3, 5, 5, 7, 9, 9, 9};
        int[] count = new int[input.length];
-       int[] register = new int[input.length];
+
         for(int x : input){
             count[x] = 1;
         }
