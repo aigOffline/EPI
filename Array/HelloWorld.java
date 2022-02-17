532b1dd31851d72de2b6acc8c88d1a941db025e6
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-16 22:24:18 -0500

    Update HelloWorld.java

--- Array/HelloWorld.java
+++ Array/HelloWorld.java
@@ -1,12 +1,17 @@
-//am9634
-//Aigerim Madakimova
+public class MyClass {
+   public static void main(String args[]){
+       int[] input = {  1, 2, 3, 4,6};
+     
+       	for(int i = 0; i < input.length/2; i++){
+       	    int temp = input[i];
+       	    input[i] = input[input.length-1];
+       	    input[input.length-1] = temp;
+       	}
+        System.out.println("Reversed array is: \n");
+        for (int k = 0; k < input.length; k++) {
+            System.out.print(input[k]+" ");
+        }
+      
+    }
 
-public class HelloWorld {
-
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
 }
-
-
