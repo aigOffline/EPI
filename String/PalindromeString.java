fe072d0bf235dbd99f2825accb4cdef2514b6bd4
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-17 23:50:17 -0500

    Update PalindromeString.java

--- String/PalindromeString.java
+++ String/PalindromeString.java
@@ -1,12 +1,36 @@
-//am9634
-//Aigerim Madakimova
+import java.util.*;
 
-public class HelloWorld {
+public class MyClass {
+     static boolean isPalindrome(String str) {
+         // Pointers pointing to the beginning
+        // and the end of the string
+        int i = 0, j = str.length() - 1;
+        // While there are characters to compare
+        while (i < j) {
+            // If there is a mismatch
+                if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            // Increment first pointer and
+            // decrement the other
+            i++;
+            j--;
+        }
+        // Given string is a palindrome
+        return true;
+    }
+  // Driver Code
+  public static void main (String[] args)
+  {
+    // Given array arr[]
+    String str2 = "racecar";
+    // Function Call
+   System.out.print("String 1 :");
+        if (isPalindrome(str2))
+            System.out.print("It is a palindrome");
+        else
+            System.out.print("It is not a palindrome");
 
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
+  }
+ 
 }
-
-
