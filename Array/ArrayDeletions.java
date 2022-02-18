d923267cd1d257b8ac4f2f3a96a160b7394313a1
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-17 22:12:19 -0500

    Update ArrayDeletions.java

--- Array/ArrayDeletions.java
+++ Array/ArrayDeletions.java
@@ -1,12 +1,23 @@
-//am9634
-//Aigerim Madakimova
+import java.util.*;
 
-public class HelloWorld {
-
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
+public class MyClass {
+//insert element
+     public static void main (String[] args)
+    {  
+        int[] arr = {1,2,3,4,5,6};
+        int n = arr.length;
+  
+        for(int i = 0; i<n-1; i++){
+            //we need to shift it
+            //since we going from back
+            arr[i] = arr[i+1];
+        }
+        n--;
+    
+        for(int i = 0; i < n; i++){
+            System.out.print( arr[i] + " ");
+        }
+    
+    }
+ 
 }
-
-
