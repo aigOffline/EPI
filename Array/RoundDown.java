b4e7f0297d85a77c673633170623c99ec06ff4d0
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-19 10:57:48 -0500

    Update RoundDown.java

--- Array/RoundDown.java
+++ Array/RoundDown.java
@@ -1,29 +1,30 @@
 import java.util.*;
 import java.util.Arrays;
 public class MyClass {
-     
 
+static int round(int n)
+    {
+        //472
+        //delete the last one (n/10)
+        //and (472*10) = 4720 
+    int a = (n / 10) * 10;
+
+        //4730
+    int b = (a + 10);
+    //4720 - 4727 > 4730- 4727
+
+    return (a - n > b - n )? b : a;
+    
+    }
      
     /* Driver program to test above function */
     public static void main(String[] args)
     {
-   int n=75;
-   int rem;
-       rem=n%10;
-   
-   if(rem>5)
-   {
-     n=n+(10-rem);
-   }
-   else
-   {
-    n=n-rem; 
-   }
-   System.out.println("Result is: "+ n);
-  }
-    
-
-
+         int n = 4727;
+         System.out.println(round(n));
+    }
 }
-//Result is: 70
+//4720
+
+    
 
