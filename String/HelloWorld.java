6d99134933c1dff352e031a5a0104ce7aebcbf83
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-26 14:19:18 -0500

    Update HelloWorld.java

--- String/HelloWorld.java
+++ String/HelloWorld.java
@@ -1,12 +1,48 @@
-//am9634
-//Aigerim Madakimova
-
-public class HelloWorld {
-
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
+public class MyClass {
+ 
+   
+    // This function prints the
+    // longest palindrome substring
+    // (LPS) of str[]. It also
+    // returns the length of the
+    // longest palindrome
+    public static int longestPalSubstr(String str){
+    int low, high;
+    int MaxLen = 1, start = 0;
+    int n = str.length();
+    if(n < 2)
+        return n;
+    for(int i = 0; i < n; i++){
+        high = i + 1;
+        low = i - 0;
+        while(high < n && str.charAt(high) == str.charAt(i))
+        high++;
+        while(low >= 0 && str.charAt(low) == str.charAt(i))
+        low--;
+        while(low >= 0 && high < n && str.charAt(high) == str.charAt(low)){
+        low--;
+        high++;
+        }
+        int length = high - low - 1;
+        if(MaxLen < length){
+        MaxLen = length;
+        start = low + 1;
+        }
+    }
+    System.out.println("Substring: ");
+    System.out.println(str.substring(start, start+MaxLen));
+    return MaxLen;
+        
+    }
+    
+    // Driver program to test above function
+    public static void main(String[] args)
+    {
+ 
+        String str = "babad";
+        String str1 = "cbbd";
+        System.out.println("Length is: "
+                           + longestPalSubstr(str1));
+    }
 }
-
 
