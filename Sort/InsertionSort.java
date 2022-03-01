8c07d459a0dc83bcb184c34ffe2910ba90f5a8c2
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-01 14:49:44 -0500

    Update and rename HelloWorld.java to InsertionSort.java

--- Sort/InsertionSort.java
+++ Sort/InsertionSort.java
@@ -1,26 +1,24 @@
 public class MyClass {
     public static void InsertionSort(int[] arr, int size){
-        int key = 0;
-        int j = 0;
+        //iterate array
         for(int i = 1; i < size; i++){
-            key = arr[i];
-            j = i-1;
-        
-        while(j >= 0 && key < arr[j]){
-            arr[j+1] = arr[j];
-            j = j -1;
-        }
-        arr[j+1] = key;
+            int key = arr[i];
+            int j = i-1;
+            //swap array
+            while(j >= 0 && key < arr[j]){
+                arr[j+1] = arr[j];
+                j=j-1;
+            }
+            arr[j+1] = key;
         }
     }
     public static void main(String[] args){
         int[] arr = {2,1,4,3,5,9,8};
         int n = arr.length;
-        System.out.println("Insert Sort Implementation: ");
+        System.out.println("Insertion Sort Implementation: ");
         InsertionSort(arr, n);
         for(int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
     }
 }
-
