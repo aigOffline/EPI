e3b7e49257210d06c8f5b0a5316e1874084fc553
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-03 23:36:32 -0500

    Update HelloWorld.java

--- Hashing/HelloWorld.java
+++ Hashing/HelloWorld.java
@@ -1,12 +1,32 @@
-//am9634
-//Aigerim Madakimova
-
-public class HelloWorld {
-
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
+import java.util.*;
+public class MyClass {
+      public static void print2SingleNumbers(int[] nums){
+       
+          /*We use a TreeMap to store the elements
+          in the sorted order*/
+         TreeMap<Integer, Integer> map = new TreeMap<>();
+       
+          int n = nums.length;
+       
+          /*Iterate through the array and check if each
+          element is present or not in the map. If the
+        element is present, remove it from the array
+        otherwise add it to the map*/
+       
+          for(int i = 0; i<n; i++){
+            if(map.containsKey(nums[i]))
+                  map.remove(nums[i]);
+            else
+                map.put(nums[i],1);
+        }
+       
+          System.out.println("The non-repeating integers are " + map.firstKey() + " " + map.lastKey());
+    }
+      //Driver code
+    public static void main (String[] args) {
+        int[] nums = new int[]{2,11,3,11,7,3,9,2};
+          print2SingleNumbers(nums);
+    }
+ 
 }
-
 
