3eda79b10333e7dce377da9139a13f7d714934ca
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-13 15:46:36 -0400

    Update HelloWorld.java

--- recursive/HelloWorld.java
+++ recursive/HelloWorld.java
@@ -1,12 +1,37 @@
-//am9634
-//Aigerim Madakimova
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if(n <= 1)
+        return n;
+    return (fib(n-1) + fib(n-2));
+}
+// Driver Code
+int main()
+{
+    int n = 14;
+    //14th Fibonacci number is 377
+    cout <<n <<" Fibonacci number: "  << fib(n);
+    return 0;
+}
+==============================================
+public class MyClass {
+    public static int fib(int n){
 
-public class HelloWorld {
+    if(n <= 1)
+        return n;
+    return (fib(n-1) + fib(n-2));
+    }
+    public static void main(String args[]) {
+     int n = 5;
+     //14th Fibonacci number is 377
+    System.out.println("Fibonacci series of 14 numbers is: " + fib(14));
+    // for loop to print the fibonacci series.
+    // for (int i = 0; i < n; i++)
+    // {
+    //     System.out.println(fib(i)+ " ");
+    // }
 
-	
-	public static void main(String[] argv) {
-		System.out.println("Hello world");
-	}
+    }
 }
 
 
