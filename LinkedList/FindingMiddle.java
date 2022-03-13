92ce496276c65a6edf1a62c39fe646193b7a33aa
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-13 17:32:44 -0400

    Update and rename HelloWorld.java to FindingMiddle.java

---
+++ LinkedList/FindingMiddle.java
@@ -0,0 +1,15 @@
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+        if(head==null) return -1;
+        Node slow=head, fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+}
