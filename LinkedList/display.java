dbd1799142a55749c2c58307735f1a2514801fe4
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-13 17:17:52 -0400

    Update display.java

--- LinkedList/display.java
+++ LinkedList/display.java
@@ -1,4 +1,5 @@
-   void display(Node head)
+ 
+void display(Node head)
    {
        while(head!=null)
        {
@@ -6,3 +7,17 @@
            head=head.next;
        }
    }
+
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    
+      int count=0;
+while(head){
+    head=head->next;
+    count++;
+
+}
+return count;
