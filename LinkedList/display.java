012b18e8bd67b59aa512e00ffc0091d318e2034d
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-03-15 14:08:48 -0400

    Update display.java

--- LinkedList/display.java
+++ LinkedList/display.java
@@ -1,4 +1,32 @@
- 
+
+class LinkedList{
+ Node head;
+ static class Node{
+  int data;
+  Node next;
+  Node(int d){
+   this.data = d;
+   next = null;
+  }
+  }
+ public static void printList(){
+  Node n = head;
+  while(n!= null){
+   System.out.println(n.data + " ");
+   n = next.data;
+  }
+ }
+ public static void main(String[] args){
+  LinkedList llist = new LinkedList();
+  llist.head = new Node(1);
+  llist.second = new Node(2);
+   llist.third = new  Node(3);
+  llist.head.next = second;
+  second.next = third;
+  llist.printList();
+ }
+}
+========================================================== 
 void display(Node head)
    {
        while(head!=null)
